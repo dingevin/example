1f3800fecd4251800cd59a41028b51402bae9e29
dingevin <Kevin1314IUNI@126.com>
2019-11-29 15:51:01 +0800

    Update example.py

--- example.py
+++ example.py
@@ -17,7 +17,24 @@ def dense(inputs, output_size, reuse=None, name=None):
     b = tf.get_variable('bias', [output_size], initializer=tf.zeros_initializer)
     outputs = tf.matmul(inputs, w) + b
     return outputs
-    
+
+def average_gradients(tower_grads):
+  average_grads = []
+  for grad_and_vars in zip(*tower_grads):
+    grads = []
+    # Loop over the gradients for the current variable
+    for g, _ in grad_and_vars:
+      expanded_g = tf.expand_dims(g, 0)
+      grads.append(expanded_g)
+
+    grad = tf.concat(axis=0, values=grads)
+    grad = tf.reduce_mean(grad, 0)
+
+    v = grad_and_vars[0][1]
+    grad_and_var = (grad, v)
+    average_grads.append(grad_and_var)
+  return average_grads
+
    
 with tf.device('/gpu:0'):
 
